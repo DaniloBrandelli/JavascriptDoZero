d7eea94a453cf4a6dbaf2770c1f363967e427d42
DaniloBrandelli <danilo.bran@hotmail.com>
2023-09-12 12:26:24 -0300

    exemplos da aula 1

---
+++ TIpoNumber.js
@@ -0,0 +1,19 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 2;
+const segundoNumero = 4;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Tipo Float
+
+const primeiroFloat = 2.2;
+const segundoFloat = .5;
+
+const somaFloat = primeiroFloat + segundoFloat;
+
+console.log(somaFloat);
---
+++ TipoStrings.js
@@ -0,0 +1,8 @@
+const texto1 = "Olá, mundo! ";
+const texto2 = "Deus te ama";
+const citacao = 'o Leo disse "oi!"';
+
+// conccatenação (+)
+
+const concatenacao = texto1 + texto2;
+console.log(concatenacao);
